14b9437f67cf701cb0b5c337ed519a003e63a4dd
JohnDLothbrock <an.ju.yo@outlook.com>
2026-06-05 06:54:26 -0600

    tests

--- tests/test_api_appointments.py
+++ tests/test_api_appointments.py
@@ -2,6 +2,24 @@ from fastapi.testclient import TestClient
 
 from main_api import app
 
+from app.dependencies import (
+    get_appointment_service
+)
+
+
+class MockAppointmentService:
+
+    def get_all_appointments(self):
+        return []
+
+    def get_all_appointments_with_pet(self):
+        return []
+
+
+app.dependency_overrides[
+    get_appointment_service
+] = lambda: MockAppointmentService()
+
 client = TestClient(app)
 
 
